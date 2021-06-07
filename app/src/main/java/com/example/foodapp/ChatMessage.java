8946917a5528b2fb825159e16581496cf7618e14
Chadley <chadley-cole@hotmail.co.uk>
2021-06-07 09:21:34 +0100

    Application Iteration 3

--- app/src/main/java/com/example/foodapp/ChatMessage.java
+++ app/src/main/java/com/example/foodapp/ChatMessage.java
@@ -1,26 +1,35 @@
 package com.example.foodapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -37,7 +46,13 @@ public class ChatMessage extends AppCompatActivity {
     private ImageButton SendMessageButton;
     private EditText MessageInputText;
 
+    private RecyclerView userMessagesList;
+
     CircleImageView ProfileImage;
+
+    private final List<Messages> messagesList = new ArrayList<>();
+    private LinearLayoutManager linearLayoutManager;
+    private MessageAdapter messageAdapter;
 
 
 
@@ -68,6 +83,12 @@ public class ChatMessage extends AppCompatActivity {
         Nickname.setText(messageReceiverNickname);
         Picasso.get().load(messageReceiverImage).placeholder(R.drawable.profile_image).into(ProfileImage);
 
+        messageAdapter = new MessageAdapter(messagesList);
+        userMessagesList = findViewById(R.id.list_of_messages);
+        linearLayoutManager = new LinearLayoutManager(this);
+        userMessagesList.setLayoutManager(linearLayoutManager);
+        userMessagesList.setAdapter(messageAdapter);
+
 
 
 
@@ -83,6 +104,44 @@ public class ChatMessage extends AppCompatActivity {
         });
     }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        RootRef.child("messages").child(messageSenderID).child(messageReceiverID).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Messages messages = snapshot.getValue(Messages.class);
+                messagesList.add(messages);
+
+
+                messageAdapter.notifyDataSetChanged();
+
+                userMessagesList.smoothScrollToPosition(userMessagesList.getAdapter().getItemCount());
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
 
     //Message Structure. Message SenderID -> Message ReceiverID -> UniqueMessageID -> (the message itself)
     private void SendMessage() {
